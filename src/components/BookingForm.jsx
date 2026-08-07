21c939be8e699edac077778c6b1721fe766282b3
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-08-07 00:35:23 -0700

    const

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -12,6 +12,20 @@ const initialState = {
   time: "",
    paymentScreenshot: null,
 };
+const TIME_SLOTS = [
+  "09:00 AM",
+  "10:00 AM",
+  "11:00 AM",
+  "12:00 PM",
+  "01:00 PM",
+  "02:00 PM",
+  "03:00 PM",
+  "04:00 PM",
+  "05:00 PM",
+  "06:00 PM",
+  "07:00 PM",
+];
+
 
 const FIELD_CLASS =
   "w-full bg-transparent border-b border-hairline/70 focus:border-bone outline-none py-3 font-body text-bone placeholder:text-ash transition-colors duration-300";
